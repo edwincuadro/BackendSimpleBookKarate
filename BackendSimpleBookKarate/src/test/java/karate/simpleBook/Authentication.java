4859ccaa4222b3d503334b262f2feef99e5bbfd6
Edwin Cuadro Gerlein <91037343+edwincuadro@users.noreply.github.com>
2022-08-26 10:23:31 -0500

    Fixing method name

--- BackendSimpleBookKarate/src/test/java/karate/simpleBook/Authentication.java
+++ BackendSimpleBookKarate/src/test/java/karate/simpleBook/Authentication.java
@@ -4,7 +4,7 @@ import com.intuit.karate.junit5.Karate;
 
 public class Authentication {
     @Karate.Test
-    Karate BookList(){
+    Karate Authentication(){
         return Karate.run("classpath:karate/simpleBook/authentication/authentication.feature").relativeTo(getClass());
     }
 }
